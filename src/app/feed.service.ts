54fbbe2c9cb0fd9a39b1306d5e675fd0ca1a1c18
Éderson Tiago Szlachta <ederson.szlachta@gmail.com>
2023-09-19 22:05:05 -0300

    Change to `bootstrap`

--- src/app/feed.service.ts
+++ src/app/feed.service.ts
@@ -14,13 +14,14 @@ export class FeedService {
   load() {
     let feed = localStorage.getItem('_feed');
 
-    if ( !feed )
+    if ( !feed ) {
       return this.http.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fsegurancalegal.com%2Ffeed%3Fpaged%3D1')
         .pipe(
           tap((_feed) => {
             localStorage.setItem('_feed', JSON.stringify(_feed));
           })
-        )
+        );
+    }
 
     return of(JSON.parse(feed));
   }
